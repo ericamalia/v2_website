afb73133fa911dba905dc8946cee5292f49af751
Erica <MacPro@Ericas-MacBook-Pro-4.local>
2019-02-24 15:18:44 -0800

    Initial files

---
+++ website.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+@Component({
+    selector: 'app-user-header',
+    templateUrl: './user-header.component.html',
+    styleUrls: ['./user-header.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+  })

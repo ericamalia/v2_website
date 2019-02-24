a12ad84880f957d2848562a6bf0b2c88af7853f0
Erica <MacPro@Ericas-MacBook-Pro-4.local>
2019-02-24 15:40:42 -0800

    link

--- website.ts
+++ website.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 @Component({
-    selector: 'app-user-header',
+    selector: 'v2_website',
     templateUrl: 'index.html',
     styleUrls: ['website.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush

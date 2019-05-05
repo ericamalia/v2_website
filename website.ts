ffa81b1326e5bdd2352de15f469d354b9bc10580
Erica <MacPro@Ericas-MacBook-Pro-4.local>
2019-05-05 10:51:06 -0700

    bug fix

--- website.ts
+++
@@ -1,7 +0,0 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-@Component({
-    selector: 'v2_website',
-    templateUrl: 'index.html',
-    styleUrls: ['website.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
-  })
